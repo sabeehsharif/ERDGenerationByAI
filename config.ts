1d322634aceb10e4d3f3c5f02db0cc928d616a70
sabeehsharif <Sabeesharif@gmail.com>
2023-06-14 18:56:29 +0300

    Initial commit

---
+++ config.ts
@@ -0,0 +1,9 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  //openAIKey: process.env.openAIKey,
+  openAIKey: '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',
+
+};
+
+export default config;
