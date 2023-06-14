d5261105084c7590009fa8218a6cab93ab49f113
sabeehsharif <Sabeesharif@gmail.com>
2023-06-14 18:57:55 +0300

    config update

--- config.ts
+++ config.ts
@@ -1,9 +1,7 @@
 const config = {
   botId: process.env.BOT_ID,
   botPassword: process.env.BOT_PASSWORD,
-  //openAIKey: process.env.openAIKey,
-  openAIKey: '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',
-
+  openAIKey: process.env.openAIKey,
 };
 
 export default config;
